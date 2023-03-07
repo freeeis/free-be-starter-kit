f25502761fb487ed67178217eb590f742940eb9f
ZhiquanX <x.zhiquan@gmail.com>
2023-03-07 15:31:45 +0800

    add core modules and account module by default

--- config/config.development.js
+++ config/config.development.js
@@ -3,19 +3,28 @@
  *
  * @Author: zhiquan <x.zhiquan@gmail.com>
  * @Date: 2021-08-03 09:09:24
- * @LastEditTime: 2023-03-07 15:04:46
+ * @LastEditTime: 2023-03-07 15:30:46
  * @LastEditors: zhiquan
  */
+
 module.exports = {
     modules: [
         {
             name: 'db',
             path: 'free-be-mongodb',
         },
+        'account',
         'demo',
     ],
     db: {
         autoCreateIndexes: true,
         forceDate: true,
+    },
+    account: {
+        whiteList: [
+            /^\/assets\/.*$/,
+            '/api/recover',
+            /^\/api\/register(\/.*)?$/,
+        ]
     },
 }
