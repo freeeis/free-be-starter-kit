839ad09ef4a7dcb0ebd7f11b5b5dd0c261915b85
ZhiquanX <x.zhiquan@gmail.com>
2023-03-23 15:45:34 +0800

    add draft test cases

--- config/config.test.js
+++ config/config.test.js
@@ -3,9 +3,27 @@
  *
  * @Author: zhiquan <x.zhiquan@gmail.com>
  * @Date: 2021-08-03 09:09:24
- * @LastEditTime: 2023-03-07 15:05:53
+ * @LastEditTime: 2023-03-23 15:36:19
  * @LastEditors: zhiquan
  */
 
 module.exports = {
+    modules: [
+        {
+            name: 'db',
+            path: 'free-be-mongodb',
+        },
+        'account',
+        'core-modules',
+        'demo',
+    ],
+    db: {
+        autoCreateIndexes: true,
+        forceDate: true,
+    },
+    account: {
+        whiteList: [
+            /^\/assets\/.*$/,
+        ]
+    },
 }
--- test/modules/core/core.test.js
+++ test/modules/core/core.test.js
@@ -1,1 +1,39 @@
-module.exports = {}
+const path = require('path');
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+const freeCore = require('free-be-core');
+
+const app = require(path.resolve('app.js'));
+
+/**
+ * @description Test core module
+ * 
+ **/
+describe('core', () => {
+    it('should be an object', () => {
+        expect(freeCore).to.be.an('object');
+    });
+
+    it('should have lifecycle hooks', () => {
+        expect(freeCore).has.property('onBegin');
+        expect(freeCore).has.property('onModulesReady');
+        expect(freeCore).has.property('onAppReady');
+        expect(freeCore).has.property('onRoutersReady');
+        expect(freeCore).has.property('onLoadRouters');
+        expect(freeCore).has.property('loadModules');
+        expect(freeCore).has.property('loadRouters');
+    });
+})
+
+/**
+ * @description Test app
+ * 
+ * **/
+describe('app', () => {
+    it('app has modules loaded', () => {
+        expect(app.modules).to.be.an('object');
+        expect(app.modules).has.property('account');
+        expect(app.modules).has.property('core-modules');
+        expect(app.modules).has.property('db');
+    })
+})
